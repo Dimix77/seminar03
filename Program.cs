76ec4d1f5c2aded86ca69114d5b926888d25a80a
Dimix77 <weryluckyday77@gmail.com>
2022-08-10 01:01:12 +0800

    make first shablone

--- Program.cs
+++ Program.cs
@@ -1,57 +1,11 @@
-﻿// int a = 10;
-// string b = "Hello";
-// bool = true // false;
-// float d = 15.4f;
-// double e = 15.4f;
-
-// Тип данных  int
-// даем на любом языке, жлательно на англ
-// = значение
-// string user_name = "Volodya"
-
-// = присвоить
-// == сравнить
-
-// if (истина) {условие} 
-
-while (true)
-{
-    Console.WriteLine("Run first number");
-    int number1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Run second number");
-    int number2 = Convert.ToInt32(Console.ReadLine());
-   
-
-    Console.WriteLine("Run operator +/-/*/: are end for exit");
-
-    string Operator = Console.ReadLine();
-
-    if (Operator == "end")
-    {
-        break;
-    }
-
+﻿{
     
-    if (Operator == "+")
-    {
-        Console.WriteLine(number1 + number2);
-    }
-    else if (Operator == "-")
-    {
-        Console.WriteLine(number1 - number2);
-    }
-    else if (Operator == "*")
-    {
-        Console.WriteLine(number1 * number2);
-    }
-    else if (Operator == ":")
-    {
-        Console.WriteLine(number1 / number2);
-    }
-    else if (Operator == "end")
-    {
-        break;
-    }
+    int number1 = 5;
+    int number2 = 7;
 
-}
+     if (5 < 7)
+     {
+        Console.WriteLine("Max 7 Min 5");
+     }
 
+    }
