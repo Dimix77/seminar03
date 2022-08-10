874ca04a81d0449dbdbbc06567a4487ece8027a9
Dimix77 <weryluckyday77@gmail.com>
2022-08-11 05:00:52 +0800

    end version

--- Program.cs
+++ Program.cs
@@ -13,4 +13,32 @@ Console.WriteLine("Bye");
 
 }
 
+{
+   int a = 44;
+   int b = 5;
+   int c = 78;
+   int max = 78;
 
+   if (a > max) max = a;
+   if (b > max) max = b;
+   if (c > max) max = c;
+
+Console.WriteLine("max = 78");
+Console.WriteLine("Bye");
+
+}
+
+{
+   int a = 22;
+   int b = 3;
+   int c = 9;
+   int max = 22;
+
+   if (a > max) max = a;
+   if (b > max) max = b;
+   if (c > max) max = c;
+
+Console.WriteLine("max = 22");
+Console.WriteLine("Bye");
+
+}
