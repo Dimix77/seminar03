ea7522fddf4b1d965dd54a4163a4db08011d78da
Dimix77 <weryluckyday77@gmail.com>
2022-08-11 04:53:14 +0800

    Add task 02

--- Program.cs
+++ Program.cs
@@ -1,40 +1,16 @@
 ﻿{
-    int a = 5;
-    int b = 7;
-    int max = 7;
+   int a = 2;
+   int b = 3;
+   int c = 7;
+   int max = 7;
 
-    if (a > max) max = a;
-    if (b > max) max = b;
+   if (a > max) max = a;
+   if (b > max) max = b;
+   if (c > max) max = c;
 
- Console.WriteLine( "Max = 7" );
- Console.WriteLine("Min = 5");
+Console.WriteLine("max = 7");
+Console.WriteLine("Bye");
 
- }
-
-{
-    int a = 2;
-    int b = 10;
-    int max = 10;
-
-    if (a > max) max = a;
-    if (b > max) max = b;
-
- Console.WriteLine( "Max = 10" );
- Console.WriteLine("Min = 2");
-
- }
-
-{
-    int a = -9;
-    int b = -3;
-    int max = -3;
-
-    if (a > max) max = a;
-    if (b > max) max = b;
-
- Console.WriteLine( "Max = -3" );
- Console.WriteLine("Min = -9");
-
- }
+}
 
 
