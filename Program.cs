30f27897f7b36a0835f73bf01632487dcc69bf71
Dimix77 <weryluckyday77@gmail.com>
2022-08-16 18:23:06 +0800

    Virivnil stroki

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿{
     int numberA = Convert.ToInt32(Console.ReadLine());
-    int numberB= Convert.ToInt32(Console.ReadLine());
+    int numberB = Convert.ToInt32(Console.ReadLine());
     int max = Convert.ToInt32(Console.ReadLine());
-    
 
-if (numberA < numberB)
-{
-    Console.WriteLine(numberB);
+
+    if (numberA < numberB)
+    {
+        Console.WriteLine(numberB);
+    }
+    if (numberA > numberB)
+    {
+        Console.WriteLine(numberA);
+    }
+
+
 }
-if (numberA > numberB)
-{
-    Console.WriteLine(numberA);
-}
-
-
- }
 
 
 
